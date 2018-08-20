6ad5452cf1cb73168534bc963f6afb7eee072f9d
Nazih Ahmed <nazih.dev@gmail.com>
2018-08-20 17:02:06 +0300

    more stable docker

--- buildImage.sh
+++ buildImage.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 GIT_SHORT=$(git rev-parse --short HEAD)
-echo 'building docker image: ${GIT_SHORT}'
+echo "building docker image: ${GIT_SHORT}"
 docker build -t 925898274702.dkr.ecr.us-west-2.amazonaws.com/app-intel:${GIT_SHORT} .
 docker push 925898274702.dkr.ecr.us-west-2.amazonaws.com/app-intel:${GIT_SHORT}
 echo 'image pushed to registry, image url :'
-echo '925898274702.dkr.ecr.us-west-2.amazonaws.com/app-intel:${GIT_SHORT}'
+echo "925898274702.dkr.ecr.us-west-2.amazonaws.com/app-intel:${GIT_SHORT}"
