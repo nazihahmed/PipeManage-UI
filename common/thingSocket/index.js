07e99a3fd516c01d3eb494f41c36dcfe30cdf00c
Nazih Ahmed <nazih.dev@gmail.com>
2018-09-01 08:39:35 -0400

    cleanup

--- common/thingSocket/index.js
+++ common/thingSocket/index.js
@@ -139,16 +139,13 @@ const initSocket = () => {
 
     socket.on('updateThing', ({thingName, attributes}) => {
       var params = {
-        thingName: thingName, /* required */
+        thingName,
         attributePayload: {
           attributes,
           merge: true
-        },
-        // expectedVersion: 0
+        }
       };
       iot.updateThing(params, function(err, data) {
-        // if (err) console.log(err, err.stack); // an error occurred
-        // else     console.log("success",data);           // successful response
         if(err) {
           return socket.emit(`thing/${thingName}/update/error`);
         }
