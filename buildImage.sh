12a4796fc3da31a58abc5f7e0391581bd5d6be22
Nazih Ahmed <nazih.dev@gmail.com>
2018-09-06 14:01:25 -0400

    add colors

--- buildImage.sh
+++ buildImage.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 GIT_SHORT=$(git rev-parse --short HEAD)
-echo "building Docker image: ${GIT_SHORT}"
+green=`tput setaf 2`
+red=`tput setaf 1`
+echo "building Docker image:${red} ${GIT_SHORT}${reset}"
 docker build -t 925898274702.dkr.ecr.us-west-2.amazonaws.com/app-intel:${GIT_SHORT} .
 aws ecr get-login --no-include-email | bash &&
 docker push 925898274702.dkr.ecr.us-west-2.amazonaws.com/app-intel:${GIT_SHORT} &&
 echo 'image pushed to registry, image url :' &&
-echo "925898274702.dkr.ecr.us-west-2.amazonaws.com/app-intel:${GIT_SHORT}"
+echo "${green}925898274702.dkr.ecr.us-west-2.amazonaws.com/app-intel:${GIT_SHORT}${reset}"
