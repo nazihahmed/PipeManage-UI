177a9889cf37d22f28375ceca5be84969dacb977
Nazih Ahmed <nazih.dev@gmail.com>
2018-10-11 01:12:31 +0300

    better build image

--- buildImage.sh
+++ buildImage.sh
@@ -5,7 +5,7 @@ red=`tput setaf 1`
 reset=`tput sgr0`
 echo "building Docker image:${red} ${GIT_SHORT}${reset}"
 docker build -t 925898274702.dkr.ecr.us-west-2.amazonaws.com/app-intel:${GIT_SHORT} .
-aws ecr get-login --no-include-email | bash &&
+$(aws ecr get-login --no-include-email --region us-west-2) &&
 docker push 925898274702.dkr.ecr.us-west-2.amazonaws.com/app-intel:${GIT_SHORT} &&
 echo 'image pushed to registry, image url :' &&
 echo "${green}925898274702.dkr.ecr.us-west-2.amazonaws.com/app-intel:${GIT_SHORT}${reset}" ||
